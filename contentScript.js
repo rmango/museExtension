5543a72e33f8ca86a9554d9495511320b5ffbfd0
Morgan <morgan-graham@live.com>
2019-11-19 19:25:05 -0800

    adding ability to dispatch click events

--- contentScript.js
+++ contentScript.js
@@ -16,14 +16,36 @@ cursor.onload = function() {
 };
 
 document.body.appendChild(cursor);
-//note: you can still use the cursor object to access it even after it's injected! yay!
+//note: you can still use the cursor object to access it even after it's injected
 
 //to make the cursor move
 
-//todo add null check
+//todo add null check?
 cursor.style.top = parseInt(cursor.style.top) + 10 + 'px';
 
-setInterval(function(){
+setInterval(function() {
   cursor.style.top = parseInt(cursor.style.top) + 10 + 'px';
   cursor.style.right = parseInt(cursor.style.top) + 10 + 'px';
-}, 1000)
+
+  //click at location of cursor
+  var rect = cursor.getBoundingClientRect();
+  var ev = new MouseEvent('click', {
+    'view': window,
+    'bubbles': true,
+    'cancelable': true,
+    'screenX': rect.x,
+    'screenY': rect.y
+  });
+  var el = document.elementFromPoint(rect.x, rect.y);
+  el.dispatchEvent(ev);
+
+  
+
+}, 1000);
+
+//testing that we are actually clicking
+document.body.addEventListener('click', function() {
+  alert('click');
+}, true);
+
+
